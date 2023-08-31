9d2ea3ec50c7b9feaeabc580e8d8911822ad18ae
aminamin13 <amin.hassan.1324@gmail.com>
2023-08-31 23:02:50 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const targetDiv = document.getElementById('container1');
+
+    // Scroll to the targetDiv with smooth behavior
+    targetDiv.scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+
